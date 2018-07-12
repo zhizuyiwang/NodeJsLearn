ccd4129cad840da6146406e458c654c84a600d31
hsf <1175432154@qq.com>
2018-07-13 03:30:22 +0800

    gg

--- day02/post_formate.js
+++ day02/post_formate.js
@@ -1,6 +1,9 @@
 var http = require("http")
 var formidable = require("formidable")
 var util = require("util")
+var fs = require("fs")
+var sd = require("silly-datetime")
+var path = require("path")
 
 http.createServer(function (req,res) {
     if (req.url == "/dopost" && req.method.toLowerCase() == "post") {
@@ -13,8 +16,35 @@ http.createServer(function (req,res) {
             console.log(fields)
             console.log(files)
             console.log(util.inspect({fields: fields,files:files}))
-            res.writeHead(200,{"Content-Type":"text/html;charset=UTF8"})
-            res.end("成功")
+            //时间，使用了第三方模块，silly-datetime
+            var ttt = sd.format(new Date(), 'YYYYMMDDHHmmss');
+            var ran = parseInt(Math.random() * 89999 + 10000);
+            var extName = path.extname(files.tupian.name);
+            console.log("__dirname",__dirname)
+            //执行改名
+            var oldPath = __dirname + "/" + files.tupian.path;
+            //新的路径由三个部分组成：时间戳、随机数、拓展名
+            var newPath = __dirname + "/static/uploads/format/" + ttt + ran + extName;
+            console.log('old',oldPath)
+            console.log('newPath',newPath)
+            //改名
+            fs.rename(oldPath,newPath,function(err){
+                if(err){
+                    throw Error("改名失败");
+                }
+                res.writeHead(200, {'content-type': 'text/html;charset=UTF8'});
+                res.end("成功");
+            });
+
         })
+    }else if(req.url == "/"){
+        //呈递form.html页面
+        fs.readFile("./static/form.html",function(err,data){
+            res.writeHead(200, {'content-type': 'text/html'});
+            res.end(data);
+        })
+    }else{
+        res.writeHead(404, {'content-type': 'text/html'});
+        res.end("404");
     }
-}).listen(80,"192.168.50.195")
+}).listen(80,"127.0.0.1")
--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 var str = "1a1b1c2b";
 var reg = new RegExp("1.+2.", "g");
 console.log(reg.exec(str)[0])
+
+var person =  function Person() {
+    this
+
+}
